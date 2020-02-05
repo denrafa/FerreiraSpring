6e5230c73526cdb87717c10f0b2fe346e9657fd0
denise ferreira <d4ferreira@student.bridgew.edu>
2020-02-05 11:18:26 -0500

    test

--- DeniseFerreiraSpring1.py
+++ DeniseFerreiraSpring1.py
@@ -1,15 +1,61 @@
 import requests
 import json
+import math
 
-url='https://jobs.github.com/positions?description=python&location=new+york'
-  result = []
-  for i in range(0,226+1):
-    response = requests.get(url, params= {'list':i, 'per_list' :50})
-    if response.status_code ==226:
-        result += response.json()
-    data = response.json()
 
+response =requests.get("https://jobs.github.com/positions?description=python&location=new+york")
+print(response.status_code)
+200
+print(response.json())
 
 
 
+for i in range(1,226):
+    print(i , end = "100 ")
+    print(i-126)
+
+{'message': 'success' }
+
+
+def jprint(obj):
+    text = json.dumps(obj,sort_keys=True, indent =4)
+    print(text)
+
+jprint(response.json())
+
+{"message" : "success"
+   }
+
+parameters = {
+    "description": "github",
+     "location" : "New York",
+       "lat" : 40.71,
+        "long" : -74
+
+}
+
+
+
+
+
+def data_items(description,location,lat,long,full_time):
+    description = "python"
+    location = " New York"
+    full_time = True
+
+
+
+
+
+    print(description + " " + location + " " + lat + " " + long + " " + full_time)
+
+
+
+data_items("description", "location", " lat", "long" , " full_time")
+
+def job_title(data_analytics,software_architect,software_eng,technology):
+    print(" ")
+
+
+data_items("description", "location", "lat", "long","full_time")
 
---
+++ test_DeniseFerreiraSpring1.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_data_items(self):
+        self.fail()
